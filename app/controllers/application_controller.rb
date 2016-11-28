217adb2510090aace768b87391ccf73c2a27634c
charlesmferguson <charles.m.ferguson.jr@gmail.com>
2016-11-28 16:05:21 -0500

    modified application_controller back to default in preparation for adding home controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "¡hola, munda!"
-  end
-
-  def goodbye
-    render html: "tizaonana, munda!"
-  end 
 end
